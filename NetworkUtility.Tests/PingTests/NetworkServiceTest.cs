4a62fe93aa5b8d0ff9ae4b1aedbd4196e7176403
Prabhat Vishwakarma <83131972+PrabhatVishwakarma@users.noreply.github.com>
2023-01-24 17:22:03 +0530

    TODAY WORK

---
+++ NetworkUtility.Tests/PingTests/NetworkServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NetworkUtility.Ping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetworkUtility.Tests.PingTests
+{
+    public class NetworkServiceTest
+    {
+        [Fact]
+        public void NetworkServiceSendPingReturnString()
+        {
+            //Arrange
+            var pingService = new NetworkService();
+            //Act
+            var result = pingService.SendPing();
+            //Assert
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Ping Sent:");
+            result.Should().Contain("Ping", Exactly.Once());
+            result.Should().Contain("Sent", Exactly.Once());
+        }
+    }
+}
